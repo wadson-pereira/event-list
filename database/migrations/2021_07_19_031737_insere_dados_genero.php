4cf816fa1ee3a050667c158c488debed33511cbc
wadson Pereira <wadson.pereira@before.com.br>
2021-07-19 01:10:42 -0300

    Migrations para usuario e genero

---
+++ database/migrations/2021_07_19_031737_insere_dados_genero.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CriaRelacaoUsuarioGenero extends Migration
+{
+
+    public function up()
+    {
+        DB::beginTransaction();
+        try {
+            Schema::table('usuario', function (Blueprint $table) {
+                $table->foreign('genero_id')->references('id')->on('gereno');
+            });
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    public function down()
+    {
+        DB::beginTransaction();
+        try {
+            Schema::table('usuario', function (Blueprint $table) {
+                $table->dropForeign(['genero_id']);
+            });
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+}
